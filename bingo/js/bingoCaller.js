a16f33c863f4c607b353dad085902ba26816b1f3
Jonathan Davis Samuel <jone.bala@gmail.com>
2018-02-23 19:35:02 -0600

    added page up and page down

--- bingo/js/bingoCaller.js
+++ bingo/js/bingoCaller.js
@@ -111,7 +111,7 @@ var Bingo = function(bingoBoardElement, speechInstance) {
     }
 
 window.onkeydown = function(e) {
-   if(e.keyCode == 38 || e.keyCode == 39){
+   if(e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 33 || e.keyCode == 34){
    //console.log("test");
    callBingoBall();
  }
