8048408a855c1d4793be55fb589b897ef6021963
Jonathan Davis Samuel <jone.bala@gmail.com>
2018-02-23 19:43:33 -0600

    oh

--- bingo/js/bingoCaller.js
+++ bingo/js/bingoCaller.js
@@ -112,7 +112,7 @@ var Bingo = function(bingoBoardElement, speechInstance) {
 
 window.onkeydown = function(e) {
    if(e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 33 || e.keyCode == 34){
-   //console.log("test");
+   console.log("test");
    callBingoBall();
  }
  }
